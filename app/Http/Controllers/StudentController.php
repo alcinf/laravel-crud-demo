36a542e37128ec66d0a26e149c12ac51f1d70144
Abraham LC <alc_inf@yahoo.com.mx>
2023-05-08 21:04:53 -0600

    Student. Main Query.

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -13,7 +13,7 @@ class StudentController extends Controller
      */
     public function index()
     {
-        return view('students.index');
+        return view('students.index', ['students' => Student::all()]);
     }
 
     /**
--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -10,5 +10,49 @@
 
         <a href="{{ url('students/create') }}" class="btn btn-primary btn-sm">New record</a>
 
+        <!--
+        'enrollment' => 'required|unique:students|max:10'
+        ,'name' => 'required|max:255'
+        ,'birthdate' => 'required|date'
+        ,'phone' => 'required|'
+        ,'email' => 'nullable|email'
+        ,'level' => 'required'
+        -->
+
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Enrollment</th>
+                    <th>Name</th>
+                    <th>Birthdate</th>
+                    <th>Phone</th>
+                    <th>E-mail</th>
+                    <th>Level</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($students as $student)
+                    
+                <tr>
+                    <td>{{ $student->id }}</td>
+                    <td>{{ $student->enrollment }}</td>
+                    <td>{{ $student->name }}</td>
+                    <td>{{ $student->birthdate }}</td>
+                    <td>{{ $student->phone }}</td>
+                    <td>{{ $student->email }}</td>
+                    <td>{{ $student->level->name }}</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        
+
     </div>
 </main>
